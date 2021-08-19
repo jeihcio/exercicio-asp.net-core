3829058f9e15d62707e58672e82e38ef61821e99
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-19 17:42:21 -0300

    Criando o controller de Filme

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesAPI
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new();
+
+        [HttpPost]
+        public void adicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
