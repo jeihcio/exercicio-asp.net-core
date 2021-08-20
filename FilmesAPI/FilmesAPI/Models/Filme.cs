065ee1bf70b9e77e31b62ed8421b976ce8f0da61
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-08-20 10:50:16 -0300

    Add validação

--- FilmesAPI/FilmesAPI/Models/Filme.cs
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -1,10 +1,18 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required]
         public string Titulo { get; set; }
+
+        [Required]
         public string Diretor { get; set; }
+
         public string Genero { get; set; }
+
+        [Range(1, 600)]
         public int Duracao { get; set; }
     }
 }
